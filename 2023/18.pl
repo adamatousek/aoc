fae0b66fabef01c4fd7a1cde4a1b3bc3ab4bf94d
Adam Matoušek <me@adamat.dev>
2024-07-27 17:11:41 +0200

    2023/18: Rewrite using simplified shoelace formula

--- 2023/18.pl
+++ 2023/18.pl
@@ -2,88 +2,31 @@
 
 use v5.36;
 
-use experimental qw/builtin for_list/;
-use builtin qw/indexed/;
-
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
-
-use List::Util qw/min max sum product/;
-
-my %map;
 my ($x, $y) = (0, 0);
-my ( $xmin, $ymin, $xmax, $ymax ) = ( 'inf', 'inf', '-inf', '-inf' );
 
-my $sol_a = 0;
+my $state_a = { x => 0, y => 0, area => 1 };
+my $state_b = { x => 0, y => 0, area => 1 };
 
 while ( <> )
 {
-    my ( $dir, $len, $colour ) = /(.) (\d+) \(#(.{6})\)/;
-    my ( $dx, $dy ) = dir2vec( $dir );
-    for ( 1 .. $len )
-    {
-        $x += $dx;
-        $y += $dy;
-        $map{ "$x $y" } = $colour;
-        ++ $sol_a;
-        $xmin = min $xmin, $x;
-        $ymin = min $ymin, $y;
-        $xmax = max $xmax, $x;
-        $ymax = max $ymax, $y;
-    }
+    my ( $dir, $len, $hlen, $hdir ) = /(.) (\d+) \(#(.{5})(.)\)/;
+    shoelace( $state_a, $dir, $len );
+
+    $hdir =~ y/0123/RDLU/;
+    shoelace( $state_b, $hdir, hex $hlen );
 }
 
-say "$xmin $ymin, $xmax $ymax";
+say $state_a->{area};
+say $state_b->{area};
 
-ROW:
-for my $y ( $ymin + 1 .. $ymax - 1 )
+sub shoelace( $st, $d, $l )
 {
-    for my $x ( $xmin .. $xmax )
-    {
-        if ( defined $map{ "$x $y" } )
-        {
-            my $nx = $x + 1;
-            if ( defined $map{ "$nx $y" } )
-            {
-                next ROW;
-            }
-            else
-            {
-                $sol_a += fill( $nx, $y );
-                last ROW;
-            }
-        }
-    }
-}
+    if ( $d =~ /[LU]/ ) { $st->{area} += $l }
 
-say $sol_a;
+    if ( $d eq 'U' ) { $st->{y} += $l; return }
+    if ( $d eq 'D' ) { $st->{y} -= $l; return }
 
-
-sub dir2vec( $d )
-{
-    return  1,  0 if $d eq 'R';
-    return -1,  0 if $d eq 'L';
-    return  0,  1 if $d eq 'D';
-    return  0, -1 if $d eq 'U';
-    die;
-}
-
-sub fill( $x, $y )
-{
-    my @todo = ( $x, $y );
-    my $filled = 0;
-    while ( @todo )
-    {
-        my ( $x, $y ) = splice @todo, -2;
-        foreach ( qw/R L D U/ )
-        {
-            my ( $dx, $dy ) = dir2vec( $_ );
-            my $coords = ( $x + $dx ) . ' ' . ( $y + $dy );
-            next if defined $map{ $coords };
-            $map{ $coords } = 0;
-            ++ $filled;
-            push @todo, $x + $dx, $y + $dy;
-        }
-    }
-    return $filled;
+    $l = -$l if $d eq 'L';
+    $st->{area} += $l * $st->{y};
+    $st->{x} += $l;
 }
